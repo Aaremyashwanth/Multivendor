cf2b274c01728cfeff07ec428d77e0640973e0fb
Yashwanth Aarem <aaremyashwanth@gmail.com>
2026-01-09 17:08:12 +0530

    about route is created for testing

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const app=express()
 const port=5000
 app.get('/',(req,res)=>{res.send("hello world")})
 app.use('/home',(res,req)=>{res.send("Welcome to home page")})
+app.use('/abut',(res,req)=>{res.send("Welcome to about page")})
 app.listen(port,()=>{console.log("port is connected")})
